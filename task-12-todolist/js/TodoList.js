77c10522067dd0d6428f0ee9d7b4ca41caf2b102
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-05-18 08:50:22 +0700

    project 12 completed

---
+++ task-12-todolist/js/TodoList.js
@@ -0,0 +1,7 @@
+export default class ToDoList {
+  constructor(_id, _title, _isDone) {
+    this.id = _id;
+    this.title = _title;
+    this.isDone = _isDone;
+  }
+}
---
+++ task-12-todolist/js/validation.js
@@ -0,0 +1,11 @@
+export default class Validation {
+  constructor() {}
+  checkEmpty(value) {
+    if (value === "") {
+      alert("Empty!!!");
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
