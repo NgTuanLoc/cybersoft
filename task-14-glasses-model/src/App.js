3a957cf2b7a1d20f6fab86d7e25969f526a2846d
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-05-26 15:44:03 +0700

    add picking glasses function

--- task-14-glasses-model/src/App.js
+++ task-14-glasses-model/src/App.js
@@ -1,20 +1,30 @@
+import { Component } from "react";
+
 import "./App.css";
 import Header from "./components/header";
 import Model from "./components/model";
 import Glasses from "./components/glasses";
 import data from "./data/dataGlasses.json";
-
-function App() {
-  const glasses = data;
-  return (
-    <div className="app container-fluid px-0 d-flex flex-column overlay">
-      <Header />
-      <div className="container mt-5 p-5">
-        <Model />
-        <Glasses glassesProp={glasses} />
+const glasses = data;
+export default class App extends Component {
+  state = { targetGlasses: "" };
+  chooseGlassesHandler = (pickedItem) => {
+    this.setState({
+      targetGlasses: pickedItem,
+    });
+  };
+  render() {
+    return (
+      <div className="app container-fluid px-0 d-flex flex-column overlay">
+        <Header />
+        <div className="container mt-5 p-5">
+          <Model glasses={this.state.targetGlasses} />
+          <Glasses
+            glassesProp={glasses}
+            chooseGlassesHandler={this.chooseGlassesHandler}
+          />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default App;
--- task-14-glasses-model/src/components/glasses.js
+++ task-14-glasses-model/src/components/glasses.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
 
 export default class glasses extends Component {
+  handlingClickItem = (url) => {
+    this.props.chooseGlassesHandler(url);
+  };
   renderGlasses = (glasses) => {
     return glasses.map((item) => {
       return (
-        <div className="glasses__item" key={item.id}>
+        <div
+          className="glasses__item p-1 m-3"
+          key={item.id}
+          onClick={() => this.handlingClickItem(item.url)}
+        >
           <img src={process.env.PUBLIC_URL + item.url} alt={item.name} />
         </div>
       );
@@ -15,7 +22,7 @@ export default class glasses extends Component {
     const { glassesProp } = this.props;
 
     return (
-      <div className="glasses container mt-5 d-flex flex-wrap">
+      <div className="glasses container d-flex flex-wrap rounded justify-content-start align-items-center">
         {this.renderGlasses(glassesProp)}
       </div>
     );
