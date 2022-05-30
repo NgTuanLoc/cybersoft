1069e194f0d7f1fee1c130fe40d5256367424f1b
NgTuanLoc <18521011@gm.uit.edu.vn>
2022-05-30 10:40:11 +0700

    project 15 completed

--- task-15-shoes-store/src/App.js
+++ task-15-shoes-store/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+import products from "./data/shoesData.json";
+import ProductList from "./components/ProductList";
+import Modal from "./components/Modal";
+
+const App = () => {
+  const [modalState, setmodalState] = useState([]);
+  const openModal = (modalData) => {
+    setmodalState(modalData);
+  };
+
+  console.log(modalState);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app container-fluid">
+      <h1 className="text-center mb-5">Shoes Shop</h1>
+      <ProductList productsData={products} onClickModal={openModal} />
+      <Modal modalState={modalState} />
     </div>
   );
-}
+};
 
 export default App;
